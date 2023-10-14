925f9342bcb2d608ea6820f5e9e7319714185c09
Pete <peterllombardo@gmail.com>
2023-10-14 08:52:51 -0400

    initial code commits

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import {choice, remove} from "./helpers";
+import fruits from "./foods";
+
+
+const randomFruit = choice(fruits);
+console.log(`I’d like one ${randomFruit}, please.`);
+console.log(`Here you go: ${randomFruit}`);
+console.log("Delicious! May I have another?");
+remove(fruits, randomFruit);
+console.log(`I’m sorry, we’re all out. We have ${fruits.length} fruits left.`);
